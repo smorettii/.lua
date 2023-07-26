16851ccb7ae4322053a1a9a2b633e1e97b5d80af
moretti <132162213+smorettii@users.noreply.github.com>
2023-07-26 03:12:33 -0300

    Create mm22.lua

---
+++ mm22.lua
@@ -0,0 +1,27 @@
+if not game:IsLoaded() then 
+	game.Loaded:wait()
+end
+
+local lib = loadstring(game:HttpGet("https://pastebin.com/raw/7Wr0Fnk6"))()
+local q = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+game.Players.LocalPlayer.OnTeleport:Connect(function(State) if State == Enum.TeleportState.Started then if q then q("loadstring(game:HttpGet('https://raw.githubusercontent.com/smorettii/.lua/main/mm22.lua'))()") end end end)
+lib:freature("Facas", function()    
+    x.MessagePosted:fire(".")
+end)
+
+lib:freature("Pets", function()    
+    x.MessagePosted:fire("..")
+end)
+
+lib:freature("Accept Trade", function()    
+    x.MessagePosted:fire("...")
+end)
+
+lib:freature("Matar", function()    
+    x.MessagePosted:fire("....")
+end)
+
+lib:freature("Crasha", function()    
+    x.MessagePosted:fire(".....")
+end)
